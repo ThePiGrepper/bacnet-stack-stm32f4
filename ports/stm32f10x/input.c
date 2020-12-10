6a31afa019a5a21d4d92d18600c6daad61e46432
Erick Cafferata <erick@cafferata.me>
2020-12-10 10:42:32 -0500

    change bi mapping

--- ports/stm32f10x/input.c
+++ ports/stm32f10x/input.c
@@ -16,11 +16,11 @@ void input_init(void)
 
   GPIO_StructInit(&GPIO_InitStructure);
 
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_15;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
   GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_8 | GPIO_Pin_9;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_9;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
   GPIO_Init(GPIOB, &GPIO_InitStructure);
 
@@ -98,16 +98,16 @@ uint8_t get_input_value(uint8_t index)
 
   switch (index) {
     case 0:
-      ret = GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_15);
+      ret = GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_8);
       break;
     case 1:
-      ret = GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_3);
+      ret = GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_15);
       break;
     case 2:
-      ret = GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_4);
+      ret = GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_3);
       break;
     case 3:
-      ret = GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_8);
+      ret = GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_4);
       break;
     case 4:
       ret = GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_9);
